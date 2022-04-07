e89e707e5bd6c12170e13498305a9a5ebb28aa75
Grishin <grishinma2009@yandex.ru>
2022-04-08 01:40:02 +0300

    add funny text tampary

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 header("Cache-control: private, max-age=0");
 header("X-Test-Header: Test");
-echo date('Y');
+//echo date('Y');
 echo "\n";
-echo "Hello World!";
+echo "Прикольна?";
 //print_r($_SERVER);
 echo "\n";
 //print_r($_GET);
